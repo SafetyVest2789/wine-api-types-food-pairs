0cb5edb8eca4f3d782a69b087ec9068681e4973e
Safety Vest 2789 <jrnlife@gmail.com>
2022-06-28 17:33:45 -0400

    many changes to DB implement

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -1,14 +1,48 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+// // const cors = require('cors')
+// // app.use(cors())
+// document.querySelector('button').addEventListener('click', apiRequest)
 
-async function apiRequest(){
-    const winesName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://wine-api-for-non-experts.herokuapp.com/api/${winesName}`)
-        const data = await response.json()
+// async function apiRequest(){
+//     const winesName = document.querySelector('input').value
+//     try{
+//         const response = await fetch(`https://wine-api-for-non-experts.herokuapp.com/api/${winesName}`)
+//         const data = await response.json()
 
-        console.log(data)
-        document.querySelector('h2').innerText = data.typeWine
-    }catch(error){
-        console.log(error)
-    }
-}
+//         console.log(data)
+//         document.querySelector('h2').innerText = data.typeWine
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+
+$(document).ready(function () {
+    $('#name').autocomplete({
+        source: async function(request,response) {
+            let data= await fetch(`http://localhost:8000/search?query=${request.term}`)
+                    .then(results => results.json())
+                    .then(results => results.map(result => {
+                        return {
+                            label: result.name,
+                            value: result.name,
+                            id: result._id
+                        }
+                    }))
+                response(data)
+                //console.log(response)
+        },
+        minLength: 2,
+        select: function(event, ui) {
+            console.log(ui.item.id)
+            fetch(`http://localhost:8000/get/${ui.item.id}`)
+                .then(result => result.json())
+                .then(result => {
+                    $('#food').empty()
+                    result.food.forEach(food =>
+                        {
+                            $("#food").append(`<li>${food}</li>`)
+                        })
+                        $('img').attr('src',result.image)
+                })
+        }
+    })
+})
--- server.js
+++ server.js
@@ -1,114 +1,97 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const { Mongoclient } = require('mongodb')
 const app = express()
 const cors = require('cors')
-const PORT = 8000
+const {MongoClient, ObjectId } = require('mongodb')
+// const { response } = require('express')
+// const { request } = require('http')
 require('dotenv').config()
+const PORT = 8000
 
+//VARIABLES FOR DATABASE//
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'wines',
+    collection
+
+MongoClient.connect(dbConnectionStr)
+    .then(client =>{
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+        collection = db.collection('basic')
+    })
+
+//MIDDLEWARE SETUP FOR APP//
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(cors())
-
-const wines= {
-    'zinfandel':{
-        'typeWine': 'red',
-        'tanninsTaste': 'high',
-        'priceDollar': '5.99',
-        'foodPair':'chicken salad',
-        'otherName': 'Red zinfendel, sweet wine, high level alcohol'
-    },
-    'champagne':{
-        'typeWine': 'white',
-        'tanninsTaste': 'low',
-        'priceDollar': '9.99',
-        'foodPair':'chicken salad',
-        'otherName': 'sparkling wine, prosecco, cava, fancy drink'
-    },
-    'riesling':{
-        'typeWine': 'white',
-        'tanninsTaste': 'low',
-        'priceDollar': '4.99',
-        'foodPair':'chicken salad',
-        'otherName': 'Riesling, white wine'
-    },
-    'pinot noir':{
-        'typeWine': 'red',
-        'tanninsTaste': 'mid',
-        'priceDollar': '7.99',
-        'foodPair':'steak',
-        'otherName': 'Red Blend, Best Choice'
-    },
-    'mascato':{
-        'typeWine': 'white',
-        'tanninsTaste': 'low',
-        'priceDollar': '8.99',
-        'foodPair':'brunch',
-        'otherName': 'Sweet white, Summer drink'
-    },
-    'merlot':{
-        'typeWine': 'red',
-        'tanninsTaste': 'mid',
-        'priceDollar': '8.99',
-        'foodPair':'brunch',
-        'otherName': 'Sweet red wine, party wine'
-    },
-    'cabernet sauvignon':{
-        'typeWine': 'red',
-        'tanninsTaste': 'high',
-        'priceDollar': '9.99',
-        'foodPair':'meat, heavy meals, complex meals',
-        'otherName': 'Flavor town, complex, main ingredient in red blends'
-    },
-    'red blend':{
-        'typeWine': 'red',
-        'tanninsTaste': 'mid',
-        'priceDollar': '4.99',
-        'foodPair':'complex meals, meats, spicy foods',
-        'otherName': 'generic wine, wine for beginners'
-    },
-    'red box wine':{
-        'typeWine': 'red',
-        'tanninsTaste': 'mid',
-        'priceDollar': '12.99',
-        'foodPair':'spicy meals, frozen food meals',
-        'otherName': '3 liter wine, event party wine, spacebag wine'
-    },
-    'white box wine':{
-        'typeWine': 'white',
-        'tanninsTaste': 'mid',
-        'priceDollar': '12.99',
-        'foodPair':'light meals like chicken, seafood, fancy party cheese',
-        'otherName': '3 liter wine, event party wine, spacebag wine, '
-    },
-    // 'white box wine ':{
-    //     'typeWine': 'white',
-    //     'tanninsTaste': 'mid',
-    //     'priceDollar': '12.99',
-    //     'foodPair':'light meals like chicken, seafood, fancy party cheese',
-    //     'otherName': '3 liter wine, event party wine, spacebag wine, cheap pinot grigio'
-    // },
-    'cheap horrible choice':{
-        'typeWine': 'red',
-        'tanninsTaste': 'mid',
-        'priceDollar': '3.49',
-        'foodPair':'pizza',
-        'otherName': '2 dollar wine, gift for people you dislike'
-    },
-}
-
-app.get('/', (request, response)=> {
-    response.sendFile(__dirname + '/index.html')
-})
-
-app.get('/api/:name', (request,response)=> {
-    const winesName = request.params.name.toLowerCase()
-    if(wines[winesName]) {
-        response.json(wines[winesName])
-    } else {
-        response.json(wines['cheap horrible choice'])
+//THIS IS CRUD APP METHODS//
+// app.get('/', (request,response) =>{
+//     db.collection('basic').find().toArray()
+//         .then(data => {
+//             let wineList = data.map(item => item.name)
+//             console.log(wineList)
+//             response.render('index.ejs', { info: wineList })
+//         })
+//         .catch(error => console.error(error))
+// })
+//THis is experimental //
+app.get("/search", async (request,response) => {
+    try {
+        let result = await collection.aggregate([
+            {
+                "$search" : {
+                    "autocomplete" : {
+                        "query": `${request.query.query}`,
+                        "path": "name",
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+        //console.log(result)
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+        //console.log(error)
     }
-
 })
+
+app.get("/get/:id", async (request, response) => {
+    try {
+        let result = await collection.findOne({
+            "_id" : ObjectId(request.params.id)
+        })
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+}
+)
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is running on port ${PORT}! You betta go catch it!`)
+    console.log(`Server is running.Please get em`)
 })
+//THis is experimental test//
+
+// app.get('/', (request, response)=> {
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/api/:name', (request,response)=> {
+//     const winesName = request.params.name.toLowerCase()
+//     if(wines[winesName]) {
+//         response.json(wines[winesName])
+//     } else {
+//         response.json(wines['cheap horrible choice'])
+//     }
+
+// })
+
+// app.listen(process.env.PORT || PORT, () => {
+//     console.log(`The server is running on port ${PORT}! You betta go catch it!`)
+// })
